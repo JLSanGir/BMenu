ec1ee7d9fbe2eca9d5f785cf2deae63065b7a1ae
jlsangir <jlsangir@gmail.com>
2026-05-04 22:08:02 +0200

    Sistema completo Supabase2

--- supabase-client.js
+++ supabase-client.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = 'https://lawpjyknenhhvjlztpic.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxhd3BqeWtuZW5oaHZqbHp0cGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTYxNjUsImV4cCI6MjA5MzM3MjE2NX0.iX3kd7ZehoszddMa_Gv_sXtPngc_ugwM5S2btiCLCpw';
 
 // Inicializar cliente
-window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
